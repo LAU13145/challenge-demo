7c6bf0430d1cd3e5ac6caca9c986f5479f7d1acd
Laura Romero <Lau13145@gmail.com>
2024-10-26 10:16:06 -0500

    feat(eslint): config eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import { friendWelcome } from '../assets'
+import { CustomInput } from '../components'
 
 export default function Home() {
   return (
     <div className='flex items-center'>
       <Image src={friendWelcome} alt='Header Logo' height={120} width={120} className='md:w-40 xl:w-56 mt-8' />
+
+      <CustomInput />
     </div>
   )
 }
--- src/components/ui/atoms/CustomInput.tsx
+++ src/components/ui/atoms/CustomInput.tsx
@@ -1,10 +1,44 @@
+'use client'
+
+import { EnvelopeIcon } from '@heroicons/react/24/solid'
 import { Input } from '@nextui-org/react'
+import { useState } from 'react'
+
+const classNames = {
+  label: 'text-black/50 dark:text-white/90',
+  input: ['bg-transparent', 'text-black/90 dark:text-white/90', 'placeholder:text-default-700/50 dark:placeholder:text-white/60'],
+  innerWrapper: 'bg-transparent',
+  inputWrapper: [
+    'shadow-xl',
+    'bg-default-200/50',
+    'dark:bg-default/60',
+    'backdrop-blur-xl',
+    'backdrop-saturate-200',
+    'hover:bg-default-200/70',
+    'dark:hover:bg-default/70',
+    'group-data-[focus=true]:bg-default-200/50',
+    'dark:group-data-[focus=true]:bg-default/60',
+    '!cursor-text',
+  ],
+}
 
 export const CustomInput = () => {
+  const [value, setValue] = useState('')
+
   return (
-    <div className='flex w-full flex-wrap md:flex-nowrap gap-4'>
-      <Input type='email' label='Email' />
-      <Input type='email' label='Email' placeholder='Enter your email' />
-    </div>
+    <Input
+      type='email'
+      label='Email'
+      placeholder='Enter your email'
+      isRequired
+      isClearable
+      onClear={() => console.log('input cleared')}
+      startContent={<EnvelopeIcon className='text-2xl text-default-400 pointer-events-none flex-shrink-0' />}
+      isInvalid={true}
+      errorMessage='Please enter a valid email'
+      value={value}
+      onValueChange={setValue}
+      classNames={classNames}
+    />
   )
 }
